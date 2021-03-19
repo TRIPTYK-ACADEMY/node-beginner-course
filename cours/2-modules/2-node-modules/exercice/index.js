5be1c35975c31cf0f7621c82f133e5b2a06e745d
amauryd <deflorenne.amaury@gmail.com>
2021-03-19 16:24:00 +0000

    pus progress

--- cours/2-modules/2-node-modules/exercice/index.js
+++ cours/2-modules/2-node-modules/exercice/index.js
@@ -8,9 +8,9 @@ const { Image } = require('image-js');
 const execute = async () => {
     let image = await Image.load('cat.jpg');
     let grey = image
-        .resize({ width: 200 }) // resize the image, forcing a width of 200 pixels. The height is computed automatically to preserve the aspect ratio.
-        .rotate(180); // rotate the image clockwise by 30 degrees.
-    return grey.save('cat.png');
+        .resize({ width: 200 }) 
+        .rotate(180); 
+    await grey.save('cat.png');
 }
 
 execute().catch(console.error);
