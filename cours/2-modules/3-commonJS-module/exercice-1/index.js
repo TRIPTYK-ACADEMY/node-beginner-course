19dbc07838b7ded08cc1ea31b1324794fcf768b0
amauryD <deflorenne.amaury@gmail.com>
2021-03-22 11:57:05 +0100

    more exercices

---
+++ cours/2-modules/3-commonJS-module/exercice-1/index.js
@@ -0,0 +1,10 @@
+/**
+ * Exercice 1 :
+ * Faites un petit module qui exporte 4 fonctions et vous permet de convertir
+ *  - Votre âge en chat (votre âge / 9) 😺
+ *  - Votre âge en chien (votre âge / 7) 🐶
+ *  - L'âge de votre chien en âge humain 🕴
+ *  - L'âge de votre chat en âge humain 🕺
+ *
+ * N'oubliez d'afficher les résultats
+ */
