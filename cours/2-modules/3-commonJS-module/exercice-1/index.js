78f92b7c81cc847a32c2a0719c7470c3caa111b1
remadex <s.cardon@outlook.com>
2021-03-24 17:37:53 +0100

    correct exo

--- cours/2-modules/3-commonJS-module/exercice-1/index.js
+++ cours/2-modules/3-commonJS-module/exercice-1/index.js
@@ -1,6 +1,6 @@
 /**
  * Exercice 1 :
- * Faites un petit module qui exporte 4 fonctions et vous permet de convertir
+ * Faites un petit module qui exporte 2 fonctions et vous permet de convertir
  *  - Votre âge en chat (votre âge / 9) 😺
  *  - L'âge de votre chat en âge humain 🕺
  *
